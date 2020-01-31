d5ab3b792ded462e2ca378d3de1d884e23ee3896
Tara Sherman <taradelin@gmail.com>
2020-01-31 00:31:08 -0500

    Add files via upload

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,11 @@
-const knex = require('knex');
+// const knex = require('knex');
 
-const dbConfig = require('../knexfile.js');
+// const dbConfig = require('../knexfile.js');
 
-const environment = process.env.NODE_ENV || 'dev';
+// const environment = process.env.NODE_ENV || 'dev';
 
-module.exports = knex(dbConfig[environment]);
+// module.exports = knex(dbConfig[environment]);
+
+const dbEngine = process.env.DB || 'dev'
+const config = require('../knexfile')[dbEngine]
+module.exports = require('knex')(config)
